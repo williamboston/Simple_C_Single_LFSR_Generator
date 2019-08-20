619cd4e9770f0be742917f0599d33c2b7b783a39
William Boston <53207247+williamboston@users.noreply.github.com>
2019-08-20 15:00:49 +1000

    Add files via upload

---
+++ lfsr.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "output.h"
+
+int main(void)
+{
+    int tap_positions[r_len];
+
+
+
+    printf("\nEnter Output Length: \n");
+    int b_len;
+    scanf("%d", &b_len);
+    int binary_array[b_len];
+
+
+
+    printf("\nEnter Size of Register: \n");
+    int r_len;
+    scanf("%d", &r_len);
+    printf("\n");
+
+
+
+
+    for (int s=0; s<r_len; s++)
+    {
+        printf("Enter Initial State Value %d:\n", s+1);
+        scanf("%d", &binary_array[s]);
+    }
+    printf("\n");
+
+    
+
+
+    for (int t=0; t<r_len; t++)
+    {
+        
+        printf("Is there a tap on Value %d? (y/n)\n", t+1);
+        char c;
+        scanf(" %c", &c);
+        
+
+        if (c == 'y')
+        {
+            tap_positions[t] = 1;
+        }
+        else
+        {
+            tap_positions[t] = 0;
+        }
+    }
+
+    //call generates a string of n length via recursive XOR
+    generate_output(binary_array, tap_positions, r_len, b_len);
+
+    //////////////////////////////////////////////////////////////////////////
+    printf("\n\nLFSR OUTPUT:\n");
+
+    for (int p=0; p<b_len; p++)
+    {
+        printf("%d", binary_array[p]);
+    }
+
+    printf("\n\n");
+    //////////////////////////////////////////////////////////////////////////
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+lfsr_1: lfsr.c output.c
+	gcc -o lfsr_1 lfsr.c output.c -I.
